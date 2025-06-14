f1d0e9875b3bb76f881103a82c7d1d603c614ea9
Alexander <89853481+AlexanderShuvatov@users.noreply.github.com>
2025-06-14 11:47:10 +0400

    Добавляет вёрстку адаптивной сетки всех страниц (#6)
    * Добавляет вёрстку адаптивной сетки всех страниц
    
    * Исправляет замечания по вёрстке адаптивной сетки всех страниц

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+document.querySelector('.navigation').classList.remove('navigation--nojs');
+
+const menuToggle = document.querySelector('.navigation__toggle');
+const navigationList = document.querySelector('.navigation__list');
+
+menuToggle.addEventListener ('click', () => {
+  navigationList.classList.toggle('navigation__list--opened');
+  menuToggle.classList.toggle('navigation__toggle--active');
+});
